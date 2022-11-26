ac6f30debbbcf59126ed88f6a4af35cac76f53b4
Radu Iordache <radu_iordache_83@yahoo.com>
2022-11-26 17:25:41 +0200

    Add project files.

---
+++ Infrastructure/InMemProductRepo.cs
@@ -0,0 +1,24 @@
+﻿using Application;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class InMemProductRepo : IProduct
+    {
+        private List<Product> _products;
+        public InMemProductRepo()
+        {
+            _products = new List<Product> { };
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Application;
+using Infrastructure;
+
+Console.WriteLine("Hello, World!");
+
+IProduct productRepo = new InMemProductRepo();
+
+var allProducts = productRepo.GetAllProducts();
+
+foreach (var p in allProducts)
+{
+    Console.WriteLine($" - {p.Name}- ");
+}
