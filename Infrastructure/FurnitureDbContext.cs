a77ca0ceddff1129ab2b85d73ca1bab0b2fae052
Radu Iordache <radu_iordache_83@yahoo.com>
2022-12-06 11:50:16 +0200

    Added EF DBContext

---
+++ Infrastructure/FurnitureDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class FurnitureDbContext : DbContext
+    {
+        private const string connectionString = @"Server=(localdb)\mssqllocaldb;
+                                                    Database=MyFirstEfCoreDb;
+                                                    Trusted_Connection=True";
+        protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString);
+        }
+    }
+}
