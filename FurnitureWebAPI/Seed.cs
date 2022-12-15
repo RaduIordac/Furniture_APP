d2aaea3835da0c50d5fe3902ced84d3c47aafb58
Radu Iordache <radu_iordache_83@yahoo.com>
2022-12-15 10:03:24 +0200

    Added Post in webApi with mediatR

---
+++ FurnitureWebAPI/Seed.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure;
+
+namespace FurnitureWebAPI
+{
+    public class Seed
+    {
+        private readonly FurnitureDbContext _dataContext;
+        public Seed(FurnitureDbContext context)
+        {
+            _dataContext = context;
+        }
+
+    }
+}
