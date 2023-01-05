6081c7a33e34ba5b111a8bdd969f39f5880dca4b
Radu Iordache <radu_iordache_83@yahoo.com>
2023-01-05 14:48:41 +0200

    added angular project

--- Application/DTOs/ProductDto.cs
+++ Application/DTOs/ProductDto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs
 {
-    internal class ProductDto
+    public class ProductDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
